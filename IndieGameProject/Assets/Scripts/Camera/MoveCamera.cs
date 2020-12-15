bebb88ee0cf49c3b100eabba34b1f3e5f528ff77
Reynard Saputra <58236360+reysaputra16@users.noreply.github.com>
2020-12-15 19:04:05 +0100

    WalkScene done

---
+++ IndieGameProject/Assets/Scripts/Camera/MoveCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    private GameObject mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mainCamera.transform.position = mainCamera.transform.position + new Vector3(.001f, 0, 0);
+    }
+}
